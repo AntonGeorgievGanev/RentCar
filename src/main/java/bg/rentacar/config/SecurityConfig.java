f7a33d8f3c6305f6d173f053dcdbd3d2afefb0ef
Anton Ganev <122820049+AntonGeorgievGanev@users.noreply.github.com>
2024-07-12 08:09:04 +0300

    Orders scheduler for finishing approved orders.

--- src/main/java/bg/rentacar/config/SecurityConfig.java
+++ src/main/java/bg/rentacar/config/SecurityConfig.java
@@ -1,23 +1,16 @@
 package bg.rentacar.config;
 
-import bg.rentacar.web.LoginController;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.AuthenticationFailureHandler;
-import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @Configuration
 public class SecurityConfig {
-
-//    private static final Logger logger = LoggerFactory.getLogger(LoginController.class);
+    //    private static final Logger logger = LoggerFactory.getLogger(LoginController.class);
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
--- src/main/java/bg/rentacar/repository/OrderRepository.java
+++ src/main/java/bg/rentacar/repository/OrderRepository.java
@@ -1,9 +1,13 @@
 package bg.rentacar.repository;
 
 import bg.rentacar.model.entity.Order;
+import bg.rentacar.model.enums.RentOrderStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
+    List<Order> findAllByStatus(RentOrderStatus status);
 }
